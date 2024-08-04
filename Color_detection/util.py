829fb7e85132ae2a739ae08672d31f15986cf7a2
andrewf1amex <flamexnivelson@gmail.com>
2024-08-05 00:19:59 +0700

    util function of color limits added

---
+++ Color_detection/util.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+
+
+def color_limits(color):
+    color = np.uint8(color)
+    hsv_color = cv2.cvtColor(color, cv2.COLOR_BGR2HSV)
+
+    lower_limit = hsv_color[0][0][0] - 10, 100, 100
+    upper_limit = hsv_color[0][0][0] + 10, 255, 255
+
+    lower_limit = np.array(lower_limit, dtype=np.uint8)
+    upper_limit = np.array(upper_limit, dtype=np.uint8)
+
+    return lower_limit, upper_limit
+
+
