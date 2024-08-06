4364e3b6540f1ea5c29776cc7874d8e08d5b46bc
andrewf1amex <flamexnivelson@gmail.com>
2024-08-06 14:09:52 +0700

    simple face detection finished

--- Face_detection/main.py
+++ Face_detection/main.py
@@ -1,11 +1,20 @@
 import cv2
 
+face_cascade_path = cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
+face_cascade = cv2.CascadeClassifier(face_cascade_path)
 
 webcam = cv2.VideoCapture(0)
 
+
 while True:
     ret, frame = webcam.read()
-    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+    detection_frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+
+    faces = face_cascade.detectMultiScale(detection_frame, scaleFactor=1.1, minNeighbors=9, flags=0)
+
+    for x, y, w, h in faces:
+        frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 255), 1)
+
     cv2.imshow('frame', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
